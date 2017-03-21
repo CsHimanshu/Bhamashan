5cbbed0676f72c274e16a75f024ae84cd8808d76
Himanshu Singh <himanshu.singh@tothenew.com>
2017-03-21 08:53:53 +0530

    color changes

--- app/src/main/java/com/hackathon/bhamashah/MainActivity.java
+++ app/src/main/java/com/hackathon/bhamashah/MainActivity.java
@@ -1,17 +1,13 @@
 package com.hackathon.bhamashah;
 
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
